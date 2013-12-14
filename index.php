27c54a28beab0e616c391e595a2ea6f15323e7ae
svahidhoss <vahid.hosseinioun@gmail.com>
2013-12-14 14:06:50 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Comment Box
+        </title>
+        <link href="/PhpProject_CommentBox/css/layout.css" rel="stylesheet" />
+        <script type="text/javascript" src="/js/jquery.js"></script>
+        <script type="text/javascript" src="/js/jquery.js"></script>
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="page-data">
+                Page Data is here.
+            </div>
+
+            <div class="comment-wrapper">
+
+                <h3 class="comment-title"> User Feedback.....</h3>
+                <div class="comment-list">
+                    <ul class="comment-holder-ul">
+
+                        <li class="comment-holder" id="_1">
+                            <div class="user-img">
+                                <img src="./images/unnamed.jpg" class="user-img-pic"/>
+                            </div>
+
+                            <div class="comment-body">
+                                <h3 class="username-field">
+                                    Vahid Hosseinioun
+                                </h3>
+
+                                <div class="comment-text">
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                    The person's comment goes here.
+                                </div>
+                            </div>
+
+                        </li>
+                        
+                        <li class="comment-holder" id="_1">
+                            <div class="user-img">
+                                <img src="./images/unnamed.jpg" class="user-img-pic"/>
+                            </div>
+
+                            <div class="comment-body">
+                                <h3 class="username-field">
+                                    Vahid Hosseinioun
+                                </h3>
+
+                                <div class="comment-text">
+                                    The person's comment goes here.
+                                </div>
+                            </div>
+
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+    </body>
+</html>
